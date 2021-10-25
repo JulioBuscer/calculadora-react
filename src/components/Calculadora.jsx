4dae7e349454b643c42f710743af87cef4c47d4f
JulioBuscer <juliobuscer@gmail.com>
2021-10-25 00:02:47 -0500

    up

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Calculadora = () => {
+    return (
+        <div>
+            <label htmlFor="">
+                Número 1: <input type="text" />
+            </label>
+        </div>
+    )
+}
+
+export default Calculadora
+
